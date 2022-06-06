d28745d789f54f39d9aae241467a4d4dc794eabb
anaskaysar <kaysarul.apurba@northsouth.edu>
2022-06-06 10:14:01 +0600

    The Second Details Portion Working

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,4 +1,10 @@
-import { View, StyleSheet, SafeAreaView, ScrollView, Pressable } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Pressable,
+} from "react-native";
 import React from "react";
 import PlanetHeader from "../components/text/PlanetHeader";
 import Text from "../components/text/text";
@@ -15,9 +21,21 @@ import {
   VenusSvg,
 } from "../svg";
 
+const PlanetSection = ({ title, value }) => {
+  return (
+    <View style={styles.planetSection}>
+      <Text preset="small" style={{ textTransform: "uppercase" }}>
+        {title}
+      </Text>
+      <Text preset="h2">{value}</Text>
+    </View>
+  );
+};
+
 export default function Details({ navigation, route }) {
   const planet = route.params.planet;
-  const { name, description } = planet;
+  const { name, description, rotationTime, revolutionTime, radius, avgTemp } =
+    planet;
 
   const renderImage = (name) => {
     switch (name) {
@@ -52,9 +70,16 @@ export default function Details({ navigation, route }) {
           <Text style={styles.description}>{description}</Text>
           <Pressable style={styles.source}>
             <Text>Source : </Text>
-            <Text preset="h4" style={styles.wikipedia}> Wikipedia</Text>
+            <Text preset="h4" style={styles.wikipedia}>
+              {" "}
+              Wikipedia
+            </Text>
           </Pressable>
         </View>
+        <PlanetSection title="ROTATION TIME" value={rotationTime} />
+        <PlanetSection title="REVOLUTION TIME" value={revolutionTime} />
+        <PlanetSection title="RADIUS" value={radius} />
+        <PlanetSection title="AVERAGE TEMP." value={avgTemp} />
       </ScrollView>
     </SafeAreaView>
   );
@@ -82,17 +107,28 @@ const styles = StyleSheet.create({
   },
   description: {
     textAlign: "justify",
-    padding: spacing [2],
+    padding: spacing[2],
     marginTop: spacing[5],
-    lineHeight: 25
+    lineHeight: 25,
   },
   source: {
-    padding: spacing[8],
+    marginTop: spacing[8],
+    marginBottom: spacing[8],
     flexDirection: "row",
-    alignItems:'center'
+    alignItems: "center",
   },
   wikipedia: {
     fontWeight: "bold",
-    textDecorationLine:"underline"
-  }
+    textDecorationLine: "underline",
+  },
+  planetSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: spacing[5],
+    paddingVertical: spacing[4],
+    marginHorizontal: spacing[6],
+    marginBottom: spacing[4],
+    backgroundColor: colors.darkGrey
+  },
 });
